3e9d50216c5995bfc1df4244fa7f2ba8ac93d937
krishnamurthypranesh <pk96ishere@gmail.com>
2023-08-28 14:18:16 -0400

    Fix typo in variables

--- ws/variables.tf
+++ ws/variables.tf
@@ -1,17 +1,17 @@
-# aws provider vars
-var "aws_region" {
+# aws provider variables
+variable "aws_region" {
     type = string
 }
 
-# newrelic provider vars
-var "new_relic_account_id" {
+# newrelic provider variables
+variable "new_relic_account_id" {
     type = string
 }
 
-var "new_relic_api_key" {
+variable "new_relic_api_key" {
     type = string
 }
 
-var "new_relic_region" {
+variable "new_relic_region" {
     type = string
 }
